e5aa4fc37237218c40343f3c53e22db3ff96894d
David Orcsik <davidorcsik@protonmail.com>
2020-08-06 22:55:18 +0200

    Reload daemon working, wired for info-box

--- dorm-rooms-frontend/src/app/info-box/info-box.component.ts
+++ dorm-rooms-frontend/src/app/info-box/info-box.component.ts
@@ -1,21 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
 import { DormService, People } from '../dorm.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-info-box',
   templateUrl: './info-box.component.html',
   styleUrls: ['./info-box.component.css']
 })
-export class InfoBoxComponent implements OnInit {
+export class InfoBoxComponent implements OnInit, OnDestroy {
   private dormService: DormService;
+  private reloadDaemonSubscription: Subscription;
+  private changeDetectorReference: ChangeDetectorRef;
   public person: People;
 
-  constructor(dormService: DormService) {
+  constructor(changeDetectorReference: ChangeDetectorRef, dormService: DormService) {
+    this.changeDetectorReference = changeDetectorReference;
     this.dormService = dormService;
   }
 
+  private loadData(self: InfoBoxComponent): void {
+    self.dormService.getCurrentPerson().subscribe(p => {
+      self.person = Object.assign(new People(), p);
+    });
+  }
+
   ngOnInit(): void {
-    this.dormService.getCurrentPerson().subscribe(p => this.person = Object.assign(new People(), p));
+    this.loadData(this);
+    this.reloadDaemonSubscription = this.dormService.reloadDaemonEvent.subscribe(_ => this.loadData(this));
+  }
+
+  ngOnDestroy(): void {
+    this.reloadDaemonSubscription.unsubscribe();
   }
 
 }
